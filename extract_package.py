b17a028e223b5f1ce879931397ad12e64707493d
Thomas Peugnet <thomas.peugnet.dev@gmail.com>
2023-03-07 15:16:14 +0100

    Adding error support for file not found

--- extract_package.py
+++ extract_package.py
@@ -6,9 +6,12 @@ import pprint
 def extract_data():
     """Extract data from a csv file and convert it to a json file."""
 
-    # read the json file
-    with open('files/test.json', 'r') as json_file:
-        data = json.load(json_file)
-        pprint.pprint(data)
+    # read the json file and check if the file exists
+    try:
+        with open('files/test.json', 'r') as json_file:
+            data = json.load(json_file)
+            pprint.pprint(data)
+    except FileNotFoundError:
+        print("File not found")
 
     return data
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from extract_package import extract_data
 
-extract_data()
+data = extract_data()
+
+
